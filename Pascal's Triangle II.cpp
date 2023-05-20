4be245a44d9339e95df66c21daba8ce42513f8eb
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2023-05-20 22:36:50 +0530

    Create Pascal's Triangle II.cpp

---
+++ Pascal's Triangle II.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+public:
+    vector<int> getRow(int rowIndex) 
+    {
+        vector<vector<int>> v1;
+         vector<int> v2;
+
+        for (int i=0; i<=rowIndex; i++)
+        {
+            v2.clear();
+
+            for (int j=0; j<i; j++)
+            {
+                if (j == 0)
+                {
+                    v2.push_back(1);
+                }
+                else
+                {
+                    v2.push_back(v1[i-1][j] + v1[i-1][j-1]);
+                }
+            }
+            v2.push_back(1);
+            v1.push_back(v2);
+        }
+        return v1[rowIndex];
+    }
+};
