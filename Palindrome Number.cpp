4c665e4d49203edc16d66a9165d35c89c1c0fd65
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2023-06-08 21:53:38 +0530

    Create Palindrome Number.cpp

---
+++ Palindrome Number.cpp
@@ -0,0 +1,22 @@
+class Solution 
+{
+    public:
+    bool isPalindrome(int x) 
+    {
+        int temp = x;  
+        int remainder;
+
+        long reverse = 0;
+        while (x > 0)
+        {
+            remainder = x%10;
+            reverse = reverse*10 + remainder;
+            x = x/10;
+        }
+
+        if (reverse == temp)
+        return true;
+        else 
+        return false;
+    }
+};
