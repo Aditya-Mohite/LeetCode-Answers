6a56fb9d76f9fcfcfc2f82b4094bb4361504b5a9
Aditya Mohite <91630035+Aditya-Mohite@users.noreply.github.com>
2023-06-10 23:21:35 +0530

    Create Climbing Stairs.cpp

---
+++ Climbing Stairs.cpp
@@ -0,0 +1,17 @@
+class Solution 
+{
+    public:
+    int climbStairs(int n) 
+    {
+        vector<int> v1(n+1);
+
+        v1[0] = 1;
+        v1[1] = 1;
+
+        for(int i=2; i<=n; i++)
+        {
+            v1[i] = v1[i-1] + v1[i-2];
+        }
+        return v1[n];
+    }       
+};
